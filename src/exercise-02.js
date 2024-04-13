347770173e1d46a244ec81f942c8e35f76e41c4e
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-13 13:32:54 +0000

    exercise02: all test passed

--- src/exercise-02.js
+++ src/exercise-02.js
@@ -4,12 +4,17 @@ const getFileExtension = (str) => {
     return "";
   }
   // Find the last index of the period (.)
-  const lastIndex = str.lastIndexOf('.');
+  const lastIndex = str.lastIndexOf(".");
 
   // If period (.) is not found or it's the last character, return an empty string
   if (lastIndex === -1 || lastIndex === str.length - 1) {
-    return '';
+    return "";
   }
+
+  // Extract and return the extension
+
+  const ext = str.split(".").slice(1).join(".");
+  return ext;
 };
 
 export { getFileExtension };
