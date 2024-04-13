8a26dbf511252766d11ce5bc4a52a2ec0cd30c16
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-13 13:28:36 +0000

    exercise02 file provided and  not empty test passed

--- src/exercise-02.js
+++ src/exercise-02.js
@@ -1,4 +1,8 @@
 const getFileExtension = (str) => {
+  // Check if filename is provided and not empty
+  if (!str || typeof str !== "string") {
+    return "";
+  }
 };
 
 export { getFileExtension };
