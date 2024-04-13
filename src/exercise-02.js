13ccef4a760ee3796237459e6b84579faff28742
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-13 13:31:55 +0000

    exercise02: return '' if . not found or it's the last character test passed

--- src/exercise-02.js
+++ src/exercise-02.js
@@ -3,6 +3,13 @@ const getFileExtension = (str) => {
   if (!str || typeof str !== "string") {
     return "";
   }
+  // Find the last index of the period (.)
+  const lastIndex = str.lastIndexOf('.');
+
+  // If period (.) is not found or it's the last character, return an empty string
+  if (lastIndex === -1 || lastIndex === str.length - 1) {
+    return '';
+  }
 };
 
 export { getFileExtension };
