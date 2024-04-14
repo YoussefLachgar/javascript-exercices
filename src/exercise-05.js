4e154465429ebebc97637fe9e36d24546ea95ea2
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-04-14 12:30:30 +0100

    exercise05: two test passed, return just the prefix if the original text is empty.

--- src/exercise-05.js
+++ src/exercise-05.js
@@ -1,8 +1,12 @@
 const prefixText = ( str, pre) => {
     const result = '';
+    if( str == ''){
+        return pre;
+    }
     if( !str){
         return result;
     }
+    
 };
 
 export { prefixText };
